f2940ebb8094506f75fd98a91704edd1b3216051
zhaofeng-shu33 <616545598@qq.com>
2019-11-04 02:40:24 +0800

    sudo

--- build_slurm.sh
+++ build_slurm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo sh -c 'printf "deb-src http://deb.debian.org/debian buster main contrib\n" > /etc/apt/sources.list.d/buster.list'
-apt-get update
-apt-get source slurm-llnl=18.08.5.2-1
-ls
+sudo apt-get update
+sudo apt-get source slurm-llnl=18.08.5.2-1
+
+
