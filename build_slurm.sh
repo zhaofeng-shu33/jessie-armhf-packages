0c821dc46b93cc543c9a6965dc45ab7892f1b58e
zhaofeng-shu33 <616545598@qq.com>
2019-11-04 02:45:30 +0800

    allow unauth

--- build_slurm.sh
+++ build_slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo sh -c 'printf "deb-src http://deb.debian.org/debian buster main contrib\n" > /etc/apt/sources.list.d/buster.list'
 sudo apt-get update
-sudo apt-get source slurm-llnl=18.08.5.2-1
-
+sudo apt-get --allow-unauthenticated source slurm-llnl=18.08.5.2-1
+cd slurm-llnl-18.08.5.2
+sudo apt-get build-dep -a armhf slurm-llnl=18.08.5.2-1
 
