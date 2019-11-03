9ace057360340cbcb8cea508675f05270cb5a1d3
zhaofeng-shu33 <616545598@qq.com>
2019-11-04 03:05:30 +0800

    fix permission

--- build_slurm.sh
+++ build_slurm.sh
@@ -2,9 +2,8 @@
 set -e -x
 sudo sh -c 'printf "deb-src http://deb.debian.org/debian buster main contrib\n" > /etc/apt/sources.list.d/buster.list'
 sudo apt-get update
-sudo apt-get --allow-unauthenticated source slurm-llnl=18.08.5.2-1
+apt-get --allow-unauthenticated source slurm-llnl=18.08.5.2-1
 cd slurm-llnl-18.08.5.2
-ls
 # deb-helper >= 9
 sed -i 's/11/9/g' ./debian/control
 # libmysql-dev
