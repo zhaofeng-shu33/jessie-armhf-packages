5380b6aa81123e650162a187101a3417d79bc697
zhaofeng-shu33 <616545598@qq.com>
2019-11-04 02:59:03 +0800

    prompt the command

--- build_slurm.sh
+++ build_slurm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -x
 sudo sh -c 'printf "deb-src http://deb.debian.org/debian buster main contrib\n" > /etc/apt/sources.list.d/buster.list'
 sudo apt-get update
 sudo apt-get --allow-unauthenticated source slurm-llnl=18.08.5.2-1
