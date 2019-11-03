b3c6e150d87860034320425245bcd1bc176f32b4
zhaofeng-shu33 <616545598@qq.com>
2019-11-04 02:56:41 +0800

    patch control file

--- build_slurm.sh
+++ build_slurm.sh
@@ -3,5 +3,10 @@ sudo sh -c 'printf "deb-src http://deb.debian.org/debian buster main contrib\n" 
 sudo apt-get update
 sudo apt-get --allow-unauthenticated source slurm-llnl=18.08.5.2-1
 cd slurm-llnl-18.08.5.2
-sudo apt-get build-dep -a armhf slurm-llnl=18.08.5.2-1
+# deb-helper >= 9
+sed -i 's/11/9/g' debian/control
+# libmysql-dev
+sed -i 's/default-libmysql/libmysql/g' debian/control
+sudo apt-get build-dep -aarmhf slurm-llnl=18.08.5.2-1
+dpkg-buildpackage -aarmhf -us -uc
 
