96f9b32e8ce73f44f4651180c277ad5fb05eea5a
zhaofeng-shu33 <616545598@qq.com>
2019-11-04 03:10:15 +0800

    add arch

--- build_slurm.sh
+++ build_slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -x
 sudo sh -c 'printf "deb-src http://deb.debian.org/debian buster main contrib\n" > /etc/apt/sources.list.d/buster.list'
+sudo dpkg --add-architecture armhf
 sudo apt-get update
 apt-get --allow-unauthenticated source slurm-llnl=18.08.5.2-1
 cd slurm-llnl-18.08.5.2
